01a0e190dbe4dda10c8b9cde10fe38416b318805
Muhammad Faiz <muhdfaiz@mediacliq.my>
2016-12-08 10:07:39 +0800

    Fix endpoint add deal to list error when using form-data

--- application/v1/deal_cashback_binding.go
+++ application/v1/deal_cashback_binding.go
@@ -2,6 +2,6 @@ package v1
 
 // AddDealToShoppingListAndCashback will bind request data based on header content type
 type CreateDealCashback struct {
-	ShoppingListGUID string `form:"user_guid" json:"shopping_list_guid" binding:"required,uuid5"`
-	DealGUID         string `form:"uuid" json:"deal_guid" binding:"required,uuid4"`
+	ShoppingListGUID string `form:"shopping_list_guid" json:"shopping_list_guid" binding:"required,uuid5"`
+	DealGUID         string `form:"deal_guid" json:"deal_guid" binding:"required,uuid4"`
 }
